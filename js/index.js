e904973ab1718eb2f1beccbaaa19f6fac13c7bf3
Fabio Luiz <fabioluis19341@gmail.com>
2022-07-21 20:41:38 -0300

    fix: remove bug

--- js/index.js
+++ js/index.js
@@ -1,23 +1,28 @@
-import Router from "./router.js"
-import Events from "./events.js"
-import {buttonHome, buttonAbout, buttonExplorer, page} from "./elements.js"
+import Router from './router.js'
+import Events from './events.js'
+import { buttonHome, buttonAbout, buttonExplorer, page } from './elements.js'
 
 const router = new Router()
 
-router.add("/", "pages/home.html")
-router.add("#home", "pages/home.html")
-router.add("#about", "pages/about.html")
-router.add("#explorer", "pages/explorer.html")
-router.add(404, "pages/error.html")
+router.add('/', 'pages/home.html')
+router.add('#home', 'pages/home.html')
+router.add('#about', 'pages/about.html')
+router.add('#explorer', 'pages/explorer.html')
+router.add(404, 'pages/error.html')
 
 router.handle()
 
+window.location = '#home'
 window.route = () => router.route()
 window.onpopstate = () => router.handle()
 
-const events = Events({buttonAbout, buttonHome, buttonExplorer, page})
+const events = Events({ buttonAbout, buttonHome, buttonExplorer, page })
 
 events.ButtonEvents()
 
-window.onhashchange = () => {events.ButtonEvents()};
-page.addEventListener('click', () => {events.ButtonEvents()})
+window.onhashchange = () => {
+  events.ButtonEvents()
+}
+page.addEventListener('click', () => {
+  events.ButtonEvents()
+})
